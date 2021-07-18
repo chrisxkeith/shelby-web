657e4837f544ad2ad3bda116fea146a55cdd2d1d
Christopher Keith <chris.keith@gmail.com>
2021-07-18 10:54:48 -0700

    Add enough data for testing.

--- docs/formflow.js
+++ docs/formflow.js
@@ -1,23 +1,82 @@
 $(document).ready(function() {
-  $('input:radio[name=services]').change(function() {
-    (new SelectionHandler().handle(this.value));
-  });
+  (new SelectionHandler().handle());
 });
 
 class SelectionHandler {
-  handle(button_value) {
-    switch(button_value) {
-      case'job_training':
-        alert("job_training");
-        break;
-      case'job_placement':
-        alert("job_placement");
-        break;
-      case'literacy':
-        alert("literacy");
-        break;
-      default:
-        alert('Unknown value: ' + button_value);
+  constructor() {
+    this.providers = {
+      two_unique : {
+        name : "2Unique Community Salvation Foundation",
+        website : "https://www.2unique-csf.org ",
+        phone_number : "... to come ...",
+        email : "Thisis2unique@gmail.com"
+      },
+      fresh_start : {
+        name : "A Fresh Start to a New Beginning",
+        website : "https://afreshstarttoanewbeginning.org",
+        phone_number : "... to come ...",
+        email : "greennadja@yahoo.com"
+      },
+      deaf_connect : {
+        name : "DeafConnect of the Mid-South, Inc.",
+        website : "https://www.deafconnect.org",
+        phone_number : "... to come ...",
+        email : "natasha@deafconnect.org"
+      },
+      goodwill_excel : {
+        name : "Goodwill Excel Center Midsouth Inc.",
+        website : "https://www.goodwillmemphis.org/theexcelcenter",
+        phone_number : "... to come ...",
+        email : "cmolinski@goodwillmemphis.org"
+      },
+      kingdom_community : {
+        name : "Kingdom Community Builders",
+        website : "https://www.facebook.com/KingdomCommunityBuilders",
+        phone_number : "... to come ...",
+        email : "dongilbert876@gmail.com"
+        
+      },
+      priority_teachers : {
+        name : "Priority Teachers University",
+        website : "https://www.priorityteachersuniversity.com",
+        phone_number : "... to come ...",
+        email : "Tinieka41@gmail.com"
+      }     
     }
+    this.services = {
+      job_training : {
+        name : "Job training",
+        providers : [
+          this.providers.two_unique,
+          this.providers.fresh_start
+        ]
+      },
+      job_placement : {
+        name : "Job Placement",
+        providers : [
+          this.providers.two_unique,
+          this.providers.fresh_start
+        ]
+      },
+      literacy : {
+        name : "Literacy (reading and/or writing)",
+        providers : [
+          this.providers.goodwill_excel,
+          this.providers.kingdom_community,
+          this.providers.priority_teachers
+        ]
+      }
+    }
+  }
+  handle() {
+    let providerEl = $(".providers");
+    providerEl.hide();
+    let services = this.services;
+    $('input:radio[name=services]').change(function() {
+      providerEl.empty();
+      providerEl.append("<br/>");
+      providerEl.append(services[this.value].name);
+      providerEl.show();
+    });
   }
 }
